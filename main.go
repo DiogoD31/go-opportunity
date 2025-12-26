f01087ea1df40799e66e9bc79ba2248efab7fa7d
DiogoD31 <diogodanilofernandesa@gmail.com>
2025-12-26 02:30:49 -0300

    :sparkles: adicionado log customizado

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/DiogoD31/go-opportunity/router"
+import (
+	"github.com/DiogoD31/go-opportunity/config"
+	"github.com/DiogoD31/go-opportunity/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 
 	router.Initialize()
 }
